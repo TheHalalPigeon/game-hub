9cdd67d6dd3a89a7240e0c71064b370272c615cb
Ayat Khaleque Akter <akhalequeakter@gmail.com>
2024-04-17 18:10:05 +0100

    Simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/api-client";
@@ -28,7 +29,7 @@ const useGames = (gameQuery: GameQuery) =>
             page: pageParam
           }
         }),
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
